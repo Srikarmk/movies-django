78c754e41567d5c313c7f8d94428d265b10c2b63
Srikar Mk <srikarmks@gmail.com>
2022-11-15 08:45:36 +0530

    added more data

--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,25 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+data={
+    'movies':[
+        {
+            'id':1,
+            'title':'Wakanda Forever',
+            'year':2022
+        },
+        {
+            'id':2,
+            'title':'Quantumania',
+            'year':2023
+        },
+        {
+            'id':3,
+            'title':'Salaar',
+            'year':2023
+        }
+    ]
+}
 def movies(request):
-    return render(request,'movies/movies.html',{'movies':['Wakanda Forever','Quantamania']})
+    return render(request,'movies/movies.html',data)
 def homepage(req):
     return HttpResponse("Home Page")
